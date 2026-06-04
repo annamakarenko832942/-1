c11683991a0fb17fe4d18c448bd170ab3df9c07d
annamakarenko832942 <annamakarenko50@gmail.com>
2026-06-04 10:46:44 +0300

    Update MainWindow.xaml.cs

--- экзамен/MainWindow.xaml.cs
+++ экзамен/MainWindow.xaml.cs
@@ -2,9 +2,7 @@
 
 namespace BisectionApp
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
